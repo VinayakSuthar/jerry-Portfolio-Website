6e794e3b13461ea1ec8df3f6eb3e500e8b0bd793
Jayesh Goswami <jayeshgoswami@carinasoftlabs.in>
2025-07-04 21:38:08 +0530

    issue fix

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://7e93a4ebfeb81a9b1c66b7bb1eb35c96@o4509016395546624.ingest.de.sentry.io/4509016400396368",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
