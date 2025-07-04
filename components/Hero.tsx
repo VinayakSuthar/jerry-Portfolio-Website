e12f7082a716167c197d27857779262c52ce8251
Jayesh Goswami <jayeshgoswami@carinasoftlabs.in>
2025-07-04 22:25:30 +0530

    Removed Three D modal issue fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,43 +32,18 @@ const Hero = () => {
 
   return (
     <div className="pb-20 pt-36" id="home">
-      {/**
-       *  UI: Spotlights
-       *  Link: https://ui.aceternity.com/components/spotlight
-       */}
-      {/* <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="yellow"
-        />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-96"
-          fill="magenta"
-        />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="cyan" />
-      </div> */}
-
-      {/**
-       *  UI: grid
-       *  change bg color to bg-black-100 and reduce grid color from
-       *  0.2 to 0.03
-       */}
       <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-dot-white/[0.5] bg-dot-black-100/[0.5]
        absolute top-0 left-0 flex items-center justify-center"
       >
-        {/* Radial gradient for the container to give a faded look */}
+        
         <div
-          // chnage the bg to bg-black-100, so it matches the bg color and will blend in
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
-        {/* <div className="absolute top-0 left-0 w-full h-full">
-          <Hero3D />
-        </div> */}
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <motion.p
             initial={{ opacity: 0, y: 50 }}
@@ -78,15 +53,7 @@ const Hero = () => {
           >
             Dynamic Web Magic with latest tech
           </motion.p>
-          {/**
-           *  Link: https://ui.aceternity.com/components/text-generate-effect
-           *
-           *  change md:text-6xl, add more responsive code
-           */}
-          {/* <TextGenerateEffect
-            words="Crafting Modern, Intuitive, and Seamless User Experiences"
-            className="text-center text-[40px] md:text-5xl lg:text-6xl"
-          /> */}
+          
           <motion.div
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
